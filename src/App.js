7ced7fc452251e5757ca42b71cc951dd7f1be82b
root <root@localhost.localdomain>
2020-10-22 18:21:01 +0530

    committed

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Board from "./components/Board";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>MineSweeper Game</h2>
+      <Board />
+    </div>
+  );
+}
+
+export default App;
